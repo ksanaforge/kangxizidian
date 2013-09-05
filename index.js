72a9b1b3de86e633bad62409aa37d5fd70c6f837
yapcheahshen <yapcheahshen@gmail.com>
2013-09-05 19:56:26 +0800

    sync with new version of aura

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 define(['underscore','backbone','aura'], function(_,Backbone,Aura) {
  console.log('loading index.js')
   var app=Aura({debug: { enable: true}});
-  app.registerWidgetsSource('aura', '../node_webkit/auraext');
+  //app.registerWidgetsSource('aura', '../node_webkit/auraext');
+  app.components.addSource('aura', '../node_webkit/auraext');
 
     app.use('../node_webkit/auraext/aura-backbone')
     .use('../node_webkit/auraext/aura-yadb')
