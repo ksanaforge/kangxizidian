1d6e2be45b10256c2e1d15981ccc7ff995301b3b
yap cheah shen <yapcheahshen@gmail.com>
2013-09-06 19:07:57 +0800

    add excerpt copy button for issue #21

--- aura_components/controls-widget/main.js
+++ aura_components/controls-widget/main.js
@@ -1,19 +1,46 @@
-define(['underscore','text!./controls.tmpl'], function(_,template) {
+-define(['underscore','text!./controls.tmpl','text!./origin.tmpl'], function(_,template,origin_template) {
   return {
     type: 'Backbone',
     events: {
       "click #btnclearall":"clearall",
       "click #btncopy":"copy",
+      "click #btnexcerptcopy":"excerptcopy"
     },
-    copy:function(e) {
-
+    checkbrowser:function(){
       var gui = require('nw.gui');
       if (!gui) {
         bootbox.alert("only node-webkit supports copy-to-clipboard<br>use ctrl+c or right-click menu",function(){})
-        return;
+        return true;
+      }      
+      return false;
+    },
+    excerptcopy:function(e) {
+      if (this.checkbrowser())return;
+      var clipboard = require('nw.gui').Clipboard.get();
+      // Or write something
+      var range=window.rangy.getSelection().getRangeAt(0);
+      var sel=range.toString();
+      var div=$(range.startContainer).parent();
+      while (div.length && !div.hasClass('pagetext')){
+        div=div.parent();
       }
+      var params={};
+      var wh=div.find("wh");
+      params.wh=wh.html();
+      params.wid=parseInt(wh.attr("wid"));
+      params.pagenumber=parseInt(div.parent().find("#pagenumber").html());
+      var origin=_.template(origin_template,params);
+      clipboard.set(sel+origin, 'text');
+      var oldlabel=$("#btnexcerptcopy").html();
+      $("#btnexcerptcopy").html('Copies!!');
+      setTimeout( function() {
+        $("#btnexcerptcopy").html(oldlabel)
+      },2000);      
+    },
+    copy:function(e) {
+      if (this.checkbrowser())return;
       // We can not create a clipboard, we have to receive the system clipboard
-      var clipboard = gui.Clipboard.get();
+      var clipboard = require('nw.gui').Clipboard.get();
       // Or write something
       var sel=window.rangy.getSelection().getRangeAt(0).toString();
       clipboard.set(sel, 'text');
---
+++ run.command
@@ -0,0 +1,2 @@
+cd ..
+node_webkit/osx-ia32/node-webkit.app/Contents/MacOS/node-webkit kangxizidian  --remote-debugging-port=9222 
